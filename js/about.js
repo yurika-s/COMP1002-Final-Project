ec16a4c26cba80981df2c24709a13fa0f7f63061
mariettia <156447343+mariettia@users.noreply.github.com>
2024-04-06 01:57:34 -0400

    improved code and display

--- js/about.js
+++ js/about.js
@@ -1,7 +1,7 @@
 // Description: JS for the About page
 
 // Gallery images set in an array
-const images = ["../images/aboutpage/aboutgallery1.png", "../images/aboutpage/aboutgallery2.png", "../images/aboutpage/aboutgallery3.png"]; // Add more images here
+const images = ["../images/aboutpage/aboutgallery1.png", "../images/aboutpage/aboutgallery2.png", "../images/aboutpage/aboutgallery3.png", "../images/aboutpage/aboutgallery4.png"];
 let currentIndex = 0; // track the current image index, starts at 0
 
 // Update images
@@ -12,12 +12,16 @@ function updateImage() {
 
 // event listener for previous button
 document.getElementById('prev').addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + images.length) % images.length; // calculate the index of previous image
+    if (currentIndex > 0) { // Check if not the first item
+        currentIndex--; // Move to the previous image
+    }
     updateImage(); // update the image
 });
 
 // event listener for next button
 document.getElementById('next').addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % images.length; // calculate the index of next image
+    if (currentIndex < images.length - 1) { // Check if not the last item
+        currentIndex++; // Move to the next image
+    }
     updateImage(); // update the image
 });
